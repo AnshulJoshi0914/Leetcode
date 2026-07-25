93288a858b9da1daf95ec5fcf0426adbedd5d524
AnshulJoshi0914 <159938020+AnshulJoshi0914@users.noreply.github.com>
2026-07-25 12:01:55 +0530

    Time: 63 ms (9.33%) | Memory: 84.4 MB (5.05%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int f(string word1, string word2, int i, int j, vector<vector<int>>& dp) {
-        if (i < 0)
-            return j + 1;
-        if (j < 0)
-            return i + 1;
+        if (i == 0)
+            return j;
+        if (j == 0)
+            return i;
         if (dp[i][j] != -1)
             return dp[i][j];
-        if (word1[i] == word2[j]) {
+        if (word1[i-1] == word2[j-1]) {
             return dp[i][j] = 0 + f(word1, word2, i - 1, j - 1, dp);
         }
 
@@ -19,7 +19,7 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return f(word1, word2, n-1, m-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return f(word1, word2, n, m, dp);
     }
 };
